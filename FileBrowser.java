fa7b48463993a6fda1de5b3fae0b075d3ef74843
TranDuyThuc99 <37242205+TranDuyThuc99@users.noreply.github.com>
2018-05-19 22:50:31 +0700

    Add files via upload

---
+++ FileBrowser.java
@@ -0,0 +1,525 @@
+package qq;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.Desktop;
+import java.awt.Dimension;
+import java.awt.Container;
+import java.awt.Component;
+import java.awt.Image;
+
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.border.*;
+import javax.swing.event.*;
+import javax.swing.tree.*;
+import javax.swing.table.*;
+import javax.swing.filechooser.FileSystemView;
+import javax.imageio.ImageIO;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
+import java.util.ArrayList;
+import java.io.*;
+import java.net.URL;
+
+public class FileBrowser {
+
+	/** Title of the application */
+	public static final String APP_TITLE = "FileBro";
+	/** Used to open/edit/print files. */
+	private Desktop desktop;
+	/** Provides nice icons and names for files. */
+	private FileSystemView fileSystemView;
+
+	/** currently selected File. */
+	@SuppressWarnings("unused")
+	private File currentFile;
+
+	/** Main GUI container */
+	private JPanel gui;
+
+	/** File-system tree. Built Lazily */
+	private JTree tree;
+	private DefaultTreeModel treeModel;
+
+	/** Directory listing */
+	private JTable table;
+	private JProgressBar progressBar;
+	/** Table model for File[]. */
+	private FileTableModel fileTableModel;
+	private ListSelectionListener listSelectionListener;
+	private boolean cellSizesSet = false;
+	private int rowIconPadding = 6;
+	private JButton printFile;
+
+	/* File details. */
+	private JLabel fileName;
+	private JTextField path;
+
+	/* GUI options/containers for new File/Directory creation. Created lazily. */
+	@SuppressWarnings("unused")
+	private JPanel newFilePanel;
+	@SuppressWarnings("unused")
+	private JRadioButton newTypeFile;
+	@SuppressWarnings("unused")
+	private JTextField name;
+
+	FileOutputStream fos;
+	ZipOutputStream zipos;
+	FileInputStream fis;
+	ZipEntry ze;
+	File file;
+	ZipInputStream zis;
+	JFileChooser fc;
+
+	public Container getGui() {
+
+		if (gui == null) {
+			gui = new JPanel(new BorderLayout(3, 3));
+			gui.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+			fileSystemView = FileSystemView.getFileSystemView();
+			desktop = Desktop.getDesktop();
+
+			JPanel detailView = new JPanel(new BorderLayout(3, 3));
+
+			table = new JTable();
+			table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+			table.setAutoCreateRowSorter(true);
+			table.setShowVerticalLines(false);
+
+			listSelectionListener = new ListSelectionListener() {
+				@Override
+				public void valueChanged(ListSelectionEvent lse) {
+					int row = table.getSelectionModel().getLeadSelectionIndex();
+					setFileDetails(((FileTableModel) table.getModel()).getFile(row));
+				}
+			};
+			table.getSelectionModel().addListSelectionListener(listSelectionListener);
+			JScrollPane tableScroll = new JScrollPane(table);
+			Dimension d = tableScroll.getPreferredSize();
+			tableScroll.setPreferredSize(new Dimension((int) d.getWidth(), (int) d.getHeight() / 2));
+			detailView.add(tableScroll, BorderLayout.CENTER);
+
+			// the File tree
+			DefaultMutableTreeNode root = new DefaultMutableTreeNode();
+			treeModel = new DefaultTreeModel(root);
+
+			TreeSelectionListener treeSelectionListener = new TreeSelectionListener() {
+				public void valueChanged(TreeSelectionEvent tse) {
+					DefaultMutableTreeNode node = (DefaultMutableTreeNode) tse.getPath().getLastPathComponent();
+					showChildren(node);
+					setFileDetails((File) node.getUserObject());
+				}
+			};
+
+			// show the file system roots.
+			File[] roots = fileSystemView.getRoots();
+			for (File fileSystemRoot : roots) {
+				DefaultMutableTreeNode node = new DefaultMutableTreeNode(fileSystemRoot);
+				root.add(node);
+				File[] files = fileSystemView.getFiles(fileSystemRoot, true);
+				for (File file : files) {
+					if (file.isDirectory()) {
+						node.add(new DefaultMutableTreeNode(file));
+					}
+				}
+				//
+			}
+
+			tree = new JTree(treeModel);
+			tree.setRootVisible(false);
+			tree.addTreeSelectionListener(treeSelectionListener);
+			tree.setCellRenderer(new FileTreeCellRenderer());
+			tree.expandRow(0);
+			JScrollPane treeScroll = new JScrollPane(tree);
+
+			// as per trashgod tip
+			tree.setVisibleRowCount(15);
+
+			Dimension preferredSize = treeScroll.getPreferredSize();
+			Dimension widePreferred = new Dimension(200, (int) preferredSize.getHeight());
+			treeScroll.setPreferredSize(widePreferred);
+
+			// details for a File
+			JPanel fileMainDetails = new JPanel(new BorderLayout(4, 2));
+			fileMainDetails.setBorder(new EmptyBorder(0, 6, 0, 6));
+
+			JPanel fileDetailsLabels = new JPanel(new GridLayout(0, 1, 2, 2));
+			fileMainDetails.add(fileDetailsLabels, BorderLayout.WEST);
+
+			JPanel fileDetailsValues = new JPanel(new GridLayout(0, 1, 2, 1));
+			fileMainDetails.add(fileDetailsValues, BorderLayout.CENTER);
+
+			fileDetailsLabels.add(new JLabel("File", JLabel.TRAILING));
+			fileName = new JLabel();
+			fileDetailsValues.add(fileName);
+			fileDetailsLabels.add(new JLabel("Path/name", JLabel.TRAILING));
+			path = new JTextField(5);
+			path.setEditable(false);
+			fileDetailsValues.add(path);
+
+			JToolBar toolBar = new JToolBar();
+			// mnemonics stop working in a floated toolbar
+			toolBar.setFloatable(false);
+
+			printFile = new JButton("Nén file");
+			printFile.setMnemonic('p');
+			printFile.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent ae) {
+					String name1 = null;
+
+					String s1 = path.getText();
+					String[] words = s1.split("\\s");
+					for (String w : words) {
+						name1 = w;
+					}
+
+					try {
+						fos = new FileOutputStream(path.getText() + ".zip");
+						zipos = new ZipOutputStream(fos);
+
+						byte[] data = new byte[1024 * 200];
+						fis = new FileInputStream(path.getText());
+						ze = new ZipEntry(name1);
+						zipos.putNextEntry(ze);
+						while (fis.read(data) != -1) {
+							zipos.write(data);
+						}
+						System.out.println();
+						zipos.flush();
+
+					} catch (FileNotFoundException e) {
+						e.printStackTrace();
+					} catch (IOException e) {
+						e.printStackTrace();
+					} finally {
+						if (fis != null) {
+							try {
+								fis.close();
+							} catch (IOException e) {
+								e.printStackTrace();
+							}
+						}
+						if (zipos != null) {
+							try {
+								zipos.close();
+							} catch (IOException e) {
+								e.printStackTrace();
+							}
+						}
+						if (fos != null) {
+							try {
+								fos.close();
+							} catch (IOException e) {
+								e.printStackTrace();
+							}
+						}
+					}
+				}
+			});
+			toolBar.add(printFile);
+			printFile.setEnabled(desktop.isSupported(Desktop.Action.PRINT));
+
+			int count = fileDetailsLabels.getComponentCount();
+			for (int ii = 0; ii < count; ii++) {
+				fileDetailsLabels.getComponent(ii).setEnabled(false);
+			}
+
+			JPanel fileView = new JPanel(new BorderLayout(3, 3));
+
+			fileView.add(toolBar, BorderLayout.NORTH);
+			fileView.add(fileMainDetails, BorderLayout.CENTER);
+
+			detailView.add(fileView, BorderLayout.SOUTH);
+
+			JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, treeScroll, detailView);
+			gui.add(splitPane, BorderLayout.CENTER);
+
+			JPanel simpleOutput = new JPanel(new BorderLayout(3, 3));
+			progressBar = new JProgressBar();
+			simpleOutput.add(progressBar, BorderLayout.EAST);
+			progressBar.setVisible(false);
+
+			gui.add(simpleOutput, BorderLayout.SOUTH);
+
+		}
+		return gui;
+	}
+
+	public void showRootFile() {
+		// ensure the main files are displayed
+		tree.setSelectionInterval(0, 0);
+	}
+
+	@SuppressWarnings("unused")
+	private TreePath findTreePath(File find) {
+		for (int ii = 0; ii < tree.getRowCount(); ii++) {
+			TreePath treePath = tree.getPathForRow(ii);
+			Object object = treePath.getLastPathComponent();
+			DefaultMutableTreeNode node = (DefaultMutableTreeNode) object;
+			File nodeFile = (File) node.getUserObject();
+
+			if (nodeFile == find) {
+				return treePath;
+			}
+		}
+		// not found!
+		return null;
+	}
+
+	@SuppressWarnings("unused")
+	private void showErrorMessage(String errorMessage, String errorTitle) {
+		JOptionPane.showMessageDialog(gui, errorMessage, errorTitle, JOptionPane.ERROR_MESSAGE);
+	}
+
+	@SuppressWarnings("unused")
+	private void showThrowable(Throwable t) {
+		t.printStackTrace();
+		JOptionPane.showMessageDialog(gui, t.toString(), t.getMessage(), JOptionPane.ERROR_MESSAGE);
+		gui.repaint();
+	}
+
+	/** Update the table on the EDT */
+	private void setTableData(final File[] files) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				if (fileTableModel == null) {
+					fileTableModel = new FileTableModel();
+					table.setModel(fileTableModel);
+				}
+				table.getSelectionModel().removeListSelectionListener(listSelectionListener);
+				fileTableModel.setFiles(files);
+				table.getSelectionModel().addListSelectionListener(listSelectionListener);
+				if (!cellSizesSet) {
+					Icon icon = fileSystemView.getSystemIcon(files[0]);
+
+					// size adjustment to better account for icons
+					table.setRowHeight(icon.getIconHeight() + rowIconPadding);
+
+					setColumnWidth(0, -1);
+					setColumnWidth(3, 60);
+					table.getColumnModel().getColumn(3).setMaxWidth(120);
+
+					cellSizesSet = true;
+				}
+			}
+		});
+	}
+
+	private void setColumnWidth(int column, int width) {
+		TableColumn tableColumn = table.getColumnModel().getColumn(column);
+		if (width < 0) {
+			// use the preferred width of the header..
+			JLabel label = new JLabel((String) tableColumn.getHeaderValue());
+			Dimension preferred = label.getPreferredSize();
+			// altered 10->14 as per camickr comment.
+			width = (int) preferred.getWidth() + 14;
+		}
+		tableColumn.setPreferredWidth(width);
+		tableColumn.setMaxWidth(width);
+		tableColumn.setMinWidth(width);
+	}
+
+	/**
+	 * Add the files that are contained within the directory of this node. Thanks to
+	 * Hovercraft Full Of Eels for the SwingWorker fix.
+	 */
+	private void showChildren(final DefaultMutableTreeNode node) {
+		tree.setEnabled(false);
+		progressBar.setVisible(true);
+		progressBar.setIndeterminate(true);
+
+		SwingWorker<Void, File> worker = new SwingWorker<Void, File>() {
+			@Override
+			public Void doInBackground() {
+				File file = (File) node.getUserObject();
+				if (file.isDirectory()) {
+					File[] files = fileSystemView.getFiles(file, true); // !!
+					if (node.isLeaf()) {
+						for (File child : files) {
+							if (child.isDirectory()) {
+								publish(child);
+							}
+						}
+					}
+					setTableData(files);
+				}
+				return null;
+			}
+
+			@Override
+			protected void process(List<File> chunks) {
+				for (File child : chunks) {
+					node.add(new DefaultMutableTreeNode(child));
+				}
+			}
+
+			@Override
+			protected void done() {
+				progressBar.setIndeterminate(false);
+				progressBar.setVisible(false);
+				tree.setEnabled(true);
+			}
+		};
+		worker.execute();
+	}
+
+	/** Update the File details view with the details of this File. */
+	private void setFileDetails(File file) {
+		currentFile = file;
+		Icon icon = fileSystemView.getSystemIcon(file);
+		fileName.setIcon(icon);
+		fileName.setText(fileSystemView.getSystemDisplayName(file));
+		path.setText(file.getPath());
+
+		JFrame f = (JFrame) gui.getTopLevelAncestor();
+		if (f != null) {
+			f.setTitle(APP_TITLE + " :: " + fileSystemView.getSystemDisplayName(file));
+		}
+
+		gui.repaint();
+	}
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					// Significantly improves the look of the output in
+					// terms of the file names returned by FileSystemView!
+					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+				} catch (Exception weTried) {
+				}
+				JFrame f = new JFrame(APP_TITLE);
+				f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+				FileBrowser FileBrowser = new FileBrowser();
+				f.setContentPane(FileBrowser.getGui());
+
+				try {
+					URL urlBig = FileBrowser.getClass().getResource("fb-icon-32x32.png");
+					URL urlSmall = FileBrowser.getClass().getResource("fb-icon-16x16.png");
+					ArrayList<Image> images = new ArrayList<Image>();
+					images.add(ImageIO.read(urlBig));
+					images.add(ImageIO.read(urlSmall));
+					f.setIconImages(images);
+				} catch (Exception weTried) {
+				}
+
+				f.pack();
+				f.setLocationByPlatform(true);
+				f.setMinimumSize(f.getSize());
+				f.setVisible(true);
+
+				FileBrowser.showRootFile();
+			}
+		});
+	}
+	
+
+/** A TableModel to hold File[]. */
+@SuppressWarnings("serial")
+class FileTableModel extends AbstractTableModel {
+
+	private File[] files;
+	private FileSystemView fileSystemView = FileSystemView.getFileSystemView();
+	private String[] columns = { "Icon", "File", "Path/name", "Size",
+
+	};
+
+	FileTableModel() {
+		this(new File[0]);
+	}
+
+	FileTableModel(File[] files) {
+		this.files = files;
+	}
+
+	public Object getValueAt(int row, int column) {
+		File file = files[row];
+		switch (column) {
+		case 0:
+			return fileSystemView.getSystemIcon(file);
+		case 1:
+			return fileSystemView.getSystemDisplayName(file);
+		case 2:
+			return file.getPath();
+		case 3:
+			return file.length();
+		default:
+			System.err.println("Logic Error");
+		}
+		return "";
+	}
+
+	public int getColumnCount() {
+		return columns.length;
+	}
+
+	public Class<?> getColumnClass(int column) {
+		switch (column) {
+		case 0:
+			return ImageIcon.class;
+		case 3:
+			return Long.class;
+
+		}
+		return String.class;
+	}
+
+	public String getColumnName(int column) {
+		return columns[column];
+	}
+
+	public int getRowCount() {
+		return files.length;
+	}
+
+	public File getFile(int row) {
+		return files[row];
+	}
+
+	public void setFiles(File[] files) {
+		this.files = files;
+		fireTableDataChanged();
+	}
+}
+
+/** A TreeCellRenderer for a File. */
+@SuppressWarnings("serial")
+class FileTreeCellRenderer extends DefaultTreeCellRenderer {
+
+	private FileSystemView fileSystemView;
+
+	private JLabel label;
+
+	FileTreeCellRenderer() {
+		label = new JLabel();
+		label.setOpaque(true);
+		fileSystemView = FileSystemView.getFileSystemView();
+	}
+
+	@Override
+	public Component getTreeCellRendererComponent(JTree tree, Object value, boolean selected, boolean expanded,
+			boolean leaf, int row, boolean hasFocus) {
+
+		DefaultMutableTreeNode node = (DefaultMutableTreeNode) value;
+		File file = (File) node.getUserObject();
+		label.setIcon(fileSystemView.getSystemIcon(file));
+		label.setText(fileSystemView.getSystemDisplayName(file));
+		label.setToolTipText(file.getPath());
+
+		if (selected) {
+			label.setBackground(backgroundSelectionColor);
+			label.setForeground(textSelectionColor);
+		} else {
+			label.setBackground(backgroundNonSelectionColor);
+			label.setForeground(textNonSelectionColor);
+		}
+
+		return label;
+	}
+}
+}
